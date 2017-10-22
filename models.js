e441be262e0137789d3918f06f14717ae77ae855
Dennis Wolff <dennisawolff@gmail.com>
2017-10-22 10:57:11 -0400

    use mongoDB

--- models.js
+++ models.js
@@ -1,71 +1,92 @@
-const uuid = require('uuid');
+const mongoose = require('mongoose');
 
-// This module provides volatile storage, using a `BlogPost`
-// model. We haven't learned about databases yet, so for now
-// we're using in-memory storage. This means each time the app stops, our storage
-// gets erased.
+// this is our schema to represent a Blog Post
+const blogPostSchema = mongoose.Schema({
+  title: {type: String, required: true},
+  content: {type: String, required: true},
+  author: {
+      firstName: {type: String, required: true},
+      lastName: {type: String, required: true}
+  },
+  publishDate: {type: Date}
+});
 
-// Don't worry too much about how BlogPost is implemented.
-// Our concern in this example is with how the API layer
-// is implemented, and getting it to use an existing model.
+blogPostSchema.virtual('author').get(function() {
+  return `${this.author.firstName} ${this.author.lastName}`.trim()});
 
+blogPostSchema.virtual('created').get(function() {
+  // let date = ${this.publishDate};
+  // date = date.getMonth()+1 + "/" + date.getDate() + "/" + date.getYear();
+  // return date;
+  return `${this.publishDate}`});
 
-function StorageException(message) {
-   this.message = message;
-   this.name = "StorageException";
+blogPostSchema.methods.apiRepr = function() {
+  return {
+    title: this.title,
+    content: this.content,
+    author: this.author,
+    created: this.created
+  };
 }
 
-const BlogPosts = {
-  create: function(title, content, author, publishDate) {
-    console.log('Creating a new blog post');
-    const post = {
-      id: uuid.v4(),
-      title: title,
-      content: content,
-      author: author,
-      publishDate: publishDate || Date.now()
-    };
-    this.items.push(post);
-    return post;
-  },
-  get: function(id=null) {
-    // if id passed in, retrieve single post,
-    // otherwise send all posts.
-    if (id !== null) {
-      return this.items.find(post => post.id === id);
-    }
-    // return posts sorted (descending) by
-    // publish date
-    return this.items.sort(function(a, b) {
-      return b.publishDate - a.publishDate
-    });
-  },
-  delete: function(id) {
-    const postIndex = this.items.findIndex(
-      post => post.id === id);
-    if (postIndex > -1) {
-      this.items.splice(postIndex, 1);
-    }
-  },
-  update: function(updatedPost) {
-    const {id} = updatedPost;
-    const postIndex = this.items.findIndex(
-      post => post.id === updatedPost.id);
-    if (postIndex === -1) {
-      throw new StorageException(
-        `Can't update item \`${id}\` because doesn't exist.`)
-    }
-    this.items[postIndex] = Object.assign(
-      this.items[postIndex], updatedPost);
-    return this.items[postIndex];
-  }
-};
+const Blog = mongoose.model('Blog', blogPostSchema);
 
-function createBlogPostsModel() {
-  const storage = Object.create(BlogPosts);
-  storage.items = [];
-  return storage;
-}
+module.exports = {Blog};
 
+// function StorageException(message) {
+//    this.message = message;
+//    this.name = "StorageException";
+// }
 
-module.exports = {BlogPosts: createBlogPostsModel()};
+// const BlogPosts = {
+//   create: function(title, content, author, publishDate) {
+//     console.log('Creating a new blog post');
+//     const post = {
+//       id: uuid.v4(),
+//       title: title,
+//       content: content,
+//       author: author,
+//       publishDate: publishDate || Date.now()
+//     };
+//     this.items.push(post);
+//     return post;
+//   },
+//   get: function(id=null) {
+//     // if id passed in, retrieve single post,
+//     // otherwise send all posts.
+//     if (id !== null) {
+//       return this.items.find(post => post.id === id);
+//     }
+//     // return posts sorted (descending) by
+//     // publish date
+//     return this.items.sort(function(a, b) {
+//       return b.publishDate - a.publishDate
+//     });
+//   },
+//   delete: function(id) {
+//     const postIndex = this.items.findIndex(
+//       post => post.id === id);
+//     if (postIndex > -1) {
+//       this.items.splice(postIndex, 1);
+//     }
+//   },
+//   update: function(updatedPost) {
+//     const {id} = updatedPost;
+//     const postIndex = this.items.findIndex(
+//       post => post.id === updatedPost.id);
+//     if (postIndex === -1) {
+//       throw new StorageException(
+//         `Can't update item \`${id}\` because doesn't exist.`)
+//     }
+//     this.items[postIndex] = Object.assign(
+//       this.items[postIndex], updatedPost);
+//     return this.items[postIndex];
+//   }
+// };
+//
+// function createBlogPostsModel() {
+//   const storage = Object.create(BlogPosts);
+//   storage.items = [];
+//   return storage;
+// }
+// module.exports = {BlogPosts: createBlogPostsModel()};
--- server.js
+++ server.js
@@ -1,56 +1,170 @@
 
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const {PORT, DATABASE_URL} = require('./config');
+const {Blog} = require('./models');
+// const blogRouter = require('./blogRouter');
+const router = express.Router();
 
 const app = express();
 
-const blogRouter = require('./blogRouter');
+mongoose.Promise = global.Promise;
 
 // log the http layer
 app.use(morgan('common'));
-
 app.use(express.static('public'));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+// GET requests to /posts => return 10 most recent blogs
+app.get('/posts', (req, res) => {
+  Blog
+    .find()
+    .limit(10)
+    // success callback: for each blog we got back, we'll
+    // call the `.apiRepr` instance method we've created in
+    // models.js in order to only expose the data we want the API return.
+    .then(blogs => {
+      res.json({
+        blogs: blogs.map(
+          (blog) => blog.apiRepr())
+      });
+    })
+    .catch(
+      err => {
+        console.error(err);
+        res.status(500).json({message: 'Internal server error'});
+    });
+});
 
-app.use('/blog-posts', blogRouter);
+// can also request by ID
+app.get('/posts/:id', (req, res) => {
+  Blog
+    // this is a convenience method Mongoose provides for searching
+    // by the object _id property
+    .findById(req.params.id)
+    .then(blog =>res.json(blog.apiRepr()))
+    .catch(err => {
+      console.error(err);
+        res.status(500).json({message: 'Internal server error'})
+    });
+});
+
+
+app.post('/posts', (req, res) => {
+  // req.body> "title": "some title",
+  //           "content": "a bunch of amazing words",
+  //           "author": {
+  //           "firstName": "Sarah",
+  //           "lastName": "Clarke" }
+  const requiredFields = ['title', 'content', 'author.firstName', 'author.lastName'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+
+  Blog
+    .create({
+      title: req.body.title,
+      content: req.body.content,
+      firstName: req.body.author.firstName,
+      lastName: req.body.author.lastName})
+    .then(
+      blog => res.status(201).json(blog.apiRepr()))
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({message: 'Internal server error'});
+    });
+});
+
+
+app.put('/posts/:id', (req, res) => {
+  // ensure that the id in the request path and the one in request body match
+  if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+    const message = (
+      `Request path id (${req.params.id}) and request body id ` +
+      `(${req.body.id}) must match`);
+    console.error(message);
+    return res.status(400).json({message: message});
+  }
+
+  // we only support a subset of fields being updateable.
+  // if the user sent over any of the updatableFields, we udpate those values
+  // in document
+  const toUpdate = {};
+  const updateableFields = ['title', 'content', 'firstName', 'lastName'];
+
+  updateableFields.forEach(field => {
+    if (field in req.body) {
+      toUpdate[field] = req.body[field];
+    }
+  });
+
+  Blog
+    // all key/value pairs in toUpdate will be updated -- that's what `$set` does
+    .findByIdAndUpdate(req.params.id, {$set: toUpdate})
+    .then(blog => res.status(200).end())
+    .catch(err => res.status(500).json({message: 'Internal server error'}));
+});
+
+app.delete('/posts/:id', (req, res) => {
+  Blog
+    .findByIdAndRemove(req.params.id)
+    .then(blog => res.status(204).end())
+    .catch(err => res.status(500).json({message: 'Internal server error'}));
+});
+
+// catch-all endpoint if client makes request to non-existent endpoint
+app.use('*', function(req, res) {
+  res.status(404).json({message: 'Not Found'});
+});
+
+
 // both runServer and closeServer need to access the same
 // server object, so we declare `server` here, and then when
 // runServer runs, it assigns a value.
 let server;
 
-// this function starts our server and returns a Promise.
-// In our test code, we need a way of asynchronously starting
-// our server, since we'll be dealing with promises there.
-function runServer() {
-  const port = process.env.PORT || 8080;
+function runServer(databaseUrl=DATABASE_URL, port=PORT) {
+
   return new Promise((resolve, reject) => {
-    server = app.listen(port, () => {
-      console.log(`Your app is listening on port ${port}`);
-      resolve(server);
-    }).on('error', err => {
-      reject(err)
+    mongoose.connect(databaseUrl, err => {
+      if (err) {
+        return reject(err);
+      }
+      server = app.listen(port, () => {
+        console.log(`Your app is listening on port ${port}`);
+        resolve();
+      })
+      .on('error', err => {
+        mongoose.disconnect();
+        reject(err);
+      });
     });
   });
 }
 
-// like `runServer`, this function also needs to return a promise.
-// `server.close` does not return a promise on its own, so we manually
-// create one.
+// this function closes the server, and returns a promise. we'll
+// use it in our integration tests later.
 function closeServer() {
-  return new Promise((resolve, reject) => {
-    console.log('Closing server');
-    server.close(err => {
-      if (err) {
-        reject(err);
-        // so we don't also call `resolve()`
-        return;
-      }
-      resolve();
-    });
+  return mongoose.disconnect().then(() => {
+     return new Promise((resolve, reject) => {
+       console.log('Closing server');
+       server.close(err => {
+           if (err) {
+               return reject(err);
+           }
+           resolve();
+       });
+     });
   });
 }
 
@@ -62,6 +176,73 @@ if (require.main === module) {
 
 module.exports = {app, runServer, closeServer};
 
-// app.listen(process.env.PORT || 8080, () => {
-//   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
+// this code from blogRouter:
+
+// BlogPosts.create(
+//   'Winnie the Pooh', 'Winnie the Pooh in modern literature is a facinating topic. Lorem ipsum dolor sit amet, ei sea labore putant epicurei, albucius convenire nam in. Te vim insolens scriptorem necessitatibus, no vim purto mutat falli, diceret accommodare et vel. Pri ea dicam incorrupte. Pri at vocent audire, dolor oportere abhorreant mel ea. Te his duis aeque doctus.', 'Bob Robertson');
+// BlogPosts.create(
+//   'Calvin and Hobbes', 'The relationship between Calvin and Hobbes is a facinating subject (not).  Ad vitae iuvaret nam, vix eu tale dicit quidam. Eum modus fabulas legendos ex, no regione delectus expetendis est, ne pri summo lobortis erroribus. Quis zril timeam eum et, sed everti pertinacia reformidans ea. Sit ut liber dicam conceptam, ut eruditi copiosae eam.', 'Gregon McGregor');
+//
+// // send back JSON representation of all blogs
+// // on GET requests to root
+// router.get('/', (req, res) => {
+//   res.json(BlogPosts.get());
 // });
+//
+//
+// // when new blog added, ensure has required fields. if not,
+// // log error and return 400 status code with hepful message.
+// // if okay, add new item, and return it with a status 201.
+// router.post('/', jsonParser, (req, res) => {
+//   // ensure `title`, `content` and `author` are in request body
+//   const requiredFields = ['title', 'content', 'author'];
+//   for (let i=0; i<requiredFields.length; i++) {
+//     const field = requiredFields[i];
+//     if (!(field in req.body)) {
+//       const message = `Missing \`${field}\` in request body`
+//       console.error(message);
+//       return res.status(400).send(message);
+//     }
+//   }
+//   const item = BlogPosts.create(req.body.title, req.body.content, req.body.author);
+//   res.status(201).json(item);
+// });
+//
+// // Delete blogs (by id)!
+// router.delete('/:id', (req, res) => {
+//   BlogPosts.delete(req.params.id);
+//   console.log(`Deleted blog post \`${req.params.ID}\``);
+//   res.status(204).end();
+// });
+//
+// // when PUT request comes in with updated blog post, ensure has
+// // required fields. also ensure that blog id in url path, and
+// // blog id in updated item object match. if problems with any
+// // of that, log error and send back status code 400. otherwise
+// // call `BlogPosts.updateItem` with updated blog post.
+// router.put('/:id', jsonParser, (req, res) => {
+//   const requiredFields = ['title', 'content', 'author'];
+//   for (let i=0; i<requiredFields.length; i++) {
+//     const field = requiredFields[i];
+//     if (!(field in req.body)) {
+//       const message = `Missing \`${field}\` in request body`
+//       console.error(message);
+//       return res.status(400).send(message);
+//     }
+//   }
+//   if (req.params.id !== req.body.id) {
+//     const message = (
+//       `Request path id (${req.params.id}) and request body id `
+//       `(${req.body.id}) must match`);
+//     console.error(message);
+//     return res.status(400).send(message);
+//   }
+//   console.log(`Updating blog post item \`${req.params.id}\``);
+//   BlogPosts.update({
+//     id: req.params.id,
+//     title: req.body.title,
+//     content: req.body.content,
+//     author: req.body.author
+//   });
+//   res.status(204).end();
+// })
